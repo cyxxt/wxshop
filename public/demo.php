7ceec0b57149df2673f38d31321c21eff919a3ff
cyxxt <853627299@qq.com>
2019-04-10 14:24:10 +0800

    aaa

---
+++ public/demo.php
@@ -0,0 +1,14 @@
+<?php
+$data=[
+    'perception'=>[
+        'inputText'=>[
+            'text'=>123
+        ],
+    ],
+    'userInfo'=>[
+        'apiKey'=>'c067dd0b957d4b68a2040463be23ae3d',
+        'userId'=>'2222'
+    ],
+];
+$post_data=json_encode($data);
+echo $post_data;
